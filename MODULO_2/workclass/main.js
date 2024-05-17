e94c176029aa02a75d7371ee16c0fe099ff24e03
Jose Portilla <daskdj@gmail.com>
2024-05-16 19:07:55 -0500

    ..

---
+++ MODULO_2/workclass/main.js
@@ -0,0 +1,11 @@
+// en el documento html escriba un texto
+document.write("hello world");
+
+var precio = 30000
+var envio = 10000
+var iva =2000
+
+
+document.write("<br> El precio total del producto es de: ", precio + envio + iva);
+
+alert("autodestruccion inminente");
