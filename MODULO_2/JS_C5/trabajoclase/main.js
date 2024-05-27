f0e376f49033a7d39545f469aae0fb002fdb5c0d
Jose Portilla <daskdj@gmail.com>
2024-05-27 17:46:40 -0500

    ..

---
+++ MODULO_2/JS_C5/trabajoclase/main.js
@@ -0,0 +1,32 @@
+// var password = prompt("digita tu contraseña")
+
+// while(password != "kambcode"){
+//     console.log("acceso denegado")
+//     password = prompt("tu contraseña es incorrecta, ingresa de nuevo")
+// }
+
+// document.write("BIENVENIDO A TU PAGINA")
+
+// var number = 1
+
+// while(number <= 50){
+//     document.write(number+1, "<br>")
+//     number =number +2
+// }
+
+// var number = 50
+
+// while(number >= 0){
+//     document.write(number, "<br>")
+//     number =number -2
+// }
+
+// for (var number =2; number <=50; number = number + 2){
+//     document.write(number, "<br>")
+// }
+
+for (var multiplo = 1; multiplo <= 10; multiplo = multiplo + 1) {
+  const producto = multiplo * 7;
+  document.write("<br>EL resultado de 7 * ", multiplo, " es: ", producto);
+}
+
